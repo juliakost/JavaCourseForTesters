5c48bb2033868b49311e5c811cc8a692e8842b5e
Yuliia Kostitsyna <yuliia.kostitsyna@aptitudesoftware.com>
2019-07-07 23:47:04 +0200

    Added possibility to run Tests in different Browsers

--- addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/TestBase.java
@@ -1,19 +1,20 @@
 package pl.ua.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pl.ua.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
-  @BeforeMethod(alwaysRun = true)
+  @BeforeMethod
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod(alwaysRun = true)
+  @AfterMethod
   public void tearDown() throws Exception {
     app.stop();
   }
