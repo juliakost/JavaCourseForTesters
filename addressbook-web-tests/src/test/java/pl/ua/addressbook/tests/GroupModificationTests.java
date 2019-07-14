cc26f186b83f5e15e6b0f71a9f848c048073a7ba
Yuliia Kostitsyna <yuliia.kostitsyna@aptitudesoftware.com>
2019-07-14 20:53:53 +0200

    Add parameter "id" to GroupData and use it in changed GroupModificationTests

--- addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.ua.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -15,13 +16,18 @@ public class GroupModificationTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("final group2", "header3", "footer3"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(before.size()- 1);
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("name", "header", "foter"));
+    GroupData group = new GroupData(before.get(before.size() -1).getId()  ,"name", "header", "foter");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
 }
