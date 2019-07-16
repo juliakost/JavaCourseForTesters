a73b5d154677328e2180b281b5274d11f1ac62da
Yuliia Kostitsyna <yuliia.kostitsyna@aptitudesoftware.com>
2019-07-16 23:25:12 +0200

    Update ContactModificationTests: to use Lists to compare before and after

--- addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/ContactModificationTests.java
@@ -4,20 +4,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.ua.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification() {
-    int before = app.getContactHelper().getContactCount();
     if (!app.getContactHelper().isContactPresent()) {
       app.getContactHelper().createContact(new ContactData("final2", "kostitsyna88", "yk2", "PL2", "Walonska 92", "123452", "543212", "67892", "98762", "juliakost22008@gmail.com", "1975", "7", "September"));
 
     }
-    app.getContactHelper().selectContract(before -1 );
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContract(before.size() -1 );
     app.getContactHelper().initContractModification();
-    app.getContactHelper().fillContactForm(new ContactData("modification", "kostitsyna345", "yk2", "PL2", "Walonska 92", "123452", "543212", "67892", "98762", "juliakost22008@gmail.com", "1975", "7", "September"));
+    ContactData contact = new ContactData("modification", "kostitsyna88", "yk2", "PL2", "Walonska 92", "123452", "543212", "67892", "98762", "juliakost22008@gmail.com", "1975", "7", "September");
+    app.getContactHelper().fillContactForm(contact);
     app.getContactHelper().submitContractModification();
     app.getContactHelper().returnToHomePage();
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after, before);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size());
   }
 }
