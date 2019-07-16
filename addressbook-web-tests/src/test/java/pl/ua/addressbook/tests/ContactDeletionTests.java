50b9e6fe43e80bcbaaaba9f434652a17dca986c7
Yuliia Kostitsyna <yuliia.kostitsyna@aptitudesoftware.com>
2019-07-16 23:11:46 +0200

    Update ContactDeletionTests: compare size of Lists before and after

--- addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/ContactDeletionTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.ua.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactDeletionTests extends TestBase {
   @Test
   public void testContactDeletion() throws InterruptedException {
@@ -11,12 +13,12 @@ public class ContactDeletionTests extends TestBase {
       app.getContactHelper().createContact(new ContactData("final2", "kostitsyna88", "yk2", "PL2", "Walonska 92", "123452", "543212", "67892", "98762", "juliakost22008@gmail.com", "1975", "7", "September"));
 
     }
-    int before = app.getContactHelper().getContactCount();
-    app.getContactHelper().selectContract( before - 1);
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContract(before.size() - 1);
     app.getContactHelper().initContractDeletion();
     app.getContactHelper().confirmOnAlert();
-    Thread.sleep(1000);
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after, before - 1);
+    Thread.sleep(5000);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() - 1);
   }
 }
