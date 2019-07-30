74b6944e52c60923981bb2aa0aa97b5a0f4a55db
Yuliia Kostitsyna <yuliia.kostitsyna@aptitudesoftware.com>
2019-07-31 00:28:05 +0200

    Add parameters to ContactDataGenerator and GroupDataGenerator

--- addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,27 @@
 package pl.ua.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.ua.addressbook.model.GroupData;
 import pl.ua.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
 
 
 public class GroupCreationTests extends TestBase {
+
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{"test1", "header1", "foter1"});
+
+    return list.iterator();
+  }
 
   @Test
   public void testGroupCreation() throws Exception {
