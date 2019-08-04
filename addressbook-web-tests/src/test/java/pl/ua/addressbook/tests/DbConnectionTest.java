96ef05c161124c490d68cc8db19badbfd9aa4cb6
Yuliia Kostitsyna <yuliia.kostitsyna@aptitudesoftware.com>
2019-08-05 00:08:06 +0200

    Connector/J: Obtaining a connection from the DriverManager

---
+++ addressbook-web-tests/src/test/java/pl/ua/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,37 @@
+package pl.ua.addressbook.tests;
+
+import org.testng.annotations.Test;
+import pl.ua.addressbook.model.GroupData;
+import pl.ua.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+
+  @Test
+  public void testDbConnection() {
+    Connection conn = null;
+    try {
+      //jdbc:mysql://localhost/db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC
+      conn = DriverManager.getConnection
+              ("jdbc:mysql://localhost:3306/addressbook?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&user=root&password=");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
+      Groups groups = new Groups();
+      while (rs.next()) {
+        groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+      System.out.println(groups);
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+}
