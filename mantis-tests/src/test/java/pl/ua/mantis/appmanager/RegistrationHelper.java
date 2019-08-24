0ae93a7d05ec25e742b03108d0688401a545a747
Yuliia Kostitsyna <yuliia.kostitsyna@aptitudesoftware.com>
2019-08-25 00:32:47 +0200

    Complete RegestrationTests

--- mantis-tests/src/test/java/pl/ua/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/pl/ua/mantis/appmanager/RegistrationHelper.java
@@ -12,6 +12,13 @@ public class RegistrationHelper extends HelperBase {
     wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
     type(By.name("username"), username);
     type(By.name("email"), email);
-    click(By.cssSelector("input[value = 'Sighup']"));
+    click(By.cssSelector("input[value = 'Signup']"));
+  }
+
+  public void finish(String confirmationLink, String password) {
+    wd.get(confirmationLink);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"), password);
+    click(By.cssSelector("button[type = 'submit']"));
   }
 }
